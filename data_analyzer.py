e965e97da324368443cf8aaebf366d0a7be9f8fe
Harish Chetty <chettyharish@gmail.com>
2014-12-04 23:43:58 -0500

    Smart RLE

--- data_analyzer.py
+++ data_analyzer.py
@@ -7,6 +7,7 @@ matplotlib.use("Agg")
 import linecache
 import matplotlib.pyplot as plt
 from operator import itemgetter
+import itertools
 
 def printTimeStatement(start_time):
     print("Current Time : {0}    Time Elapsed : {1} minutes".
@@ -14,12 +15,139 @@ def printTimeStatement(start_time):
                  (time.time() - start_time) / 60))
 
 
+def data_analyzer_smart(start_time, bench, processor, l1_size, l1_assoc, line_size):
+    printTimeStatement(start_time)
+    Storetxt = "/home/chettyharish/Downloads/Store.txt"
+    Cachetxt = "/home/chettyharish/Downloads/Cache.txt"
+    
+    total_obs = 0.0
+    with open(Storetxt, 'r') as infile:
+        total_obs +=1
+    total_obs = total_obs / 2.0
+    sampling_obs = int(total_obs / 500.0)
+    silent_stores = 0.0
+    silent_bytes = 0.0
+    total_stores = 0.0
+    total_stores_byte = 0.0
+    i = 0
+    j = 0
+    
+    #For plots
+    y1 = []
+    y2 = []
+    
+    #For RLE
+    rle_list = []
+    curr = -1
+    count = 0
+
+    with open(Storetxt, 'r') as infile:
+        Storefile = islice(infile, 2)
+        addr1 = Storefile[0].split(":")[1]
+        with open(Cachetxt, "r") as infile2:
+            Cachefile = islice(infile2 , 4)
+            addr2 = Cachefile[0].split(":")[1]
+            if addr1 == addr2:
+                cnt1 = 0
+                size = int(Cachefile[1]).split(":")[1]
+                bfdata = Cachefile[2].split("\t")
+                afdata = Cachefile[3].split("\t")
+                for k in range(size):
+                    if int(bfdata[k]) == int(afdata[k]):
+                        cnt1 += 1
+
+                if cnt1 == size:
+                    silent_stores += 1
+                    if i % sampling_obs == 0:
+                        y1.append(1)
+                silent_bytes += cnt1
+                total_stores += 1
+                total_stores_byte += cnt1
+                
+                if i % sampling_obs == 0:
+                    y2.append(float(cnt1/size))
+
+                if (cnt1 == size) == curr:
+                    count += 1
+                else:
+                    rle_list.append((i, str(curr), str(count)))
+                    rle_list = sorted(rle_list, key=itemgetter(2), reverse=True)[0:200]
+                    curr = cnt1 == size
+                    count = 1
+            else:
+                j += 4
+            i += 1
+
+    if len(silent_bytes) != 0:
+        ##############################
+        #    FINAL STATS HERE
+        #
+        ##############################
+        printTimeStatement(start_time)
+        config_string = (str(bench) + "_" + str(processor) + "_" + str(l1_size) + "KB_"
+                         + str(l1_assoc) + "_" + str(line_size))
+        opstring = (config_string + "\t" + str(total_stores) + "\t" + str(silent_stores)
+                    + "\t" + str(silent_stores / total_stores)
+                    + "\t" + str(float(silent_bytes))
+                    + "\t" + str(float(total_stores_byte))
+                    + "\t" + str(float(silent_bytes/total_stores_byte))) + "\n"
+        outputfile = open("/home/chettyharish/Downloads/op.txt", "a")
+        outputfile.write(opstring)
+        outputfile.close()
+    
+        ##############################
+        #    PLOTTING STARTS HERE
+        #
+        ##############################
+        printTimeStatement(start_time)
+        x1 = []
+
+        i = 0
+
+    
+        while i < total_obs:
+            if silent_bytes[i][1] == silent_bytes[i][2]:
+                y1.append(1)
+            else:
+                y1.append(0)
+            y2.append(float(silent_bytes[i][2]) / float(silent_bytes[i][1]))
+            i += sampling_obs
+    
+        x1 = [i for i in range(len(y1))]
+        fig = plt.figure(figsize=(22, 12), dpi = 110)
+        ax0 = plt.subplot(211)
+        ax0.plot(x1, y1)
+        ax0.set_title('Store number vs SilentStores')
+        ax1 = plt.subplot(212)
+        ax1.plot(x1, y2)
+        ax1.set_title('Store number vs Bytes Stored')
+        ax0.spines['right'].set_visible(False)
+        ax0.spines['top'].set_visible(False)
+        ax0.yaxis.set_ticks_position('left')
+        ax0.xaxis.set_ticks_position('bottom')
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['top'].set_visible(False)
+        ax1.yaxis.set_ticks_position('left')
+        ax1.xaxis.set_ticks_position('bottom')
+        plt.subplots_adjust(hspace=0.5)
+        plt.savefig("/home/chettyharish/Downloads/" + config_string + ".png")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 def data_analyzer(start_time, bench, processor, l1_size, l1_assoc, line_size):
     printTimeStatement(start_time)
-#     Storefile = [
-#         line for line in open("/home/chettyharish/Downloads/Store.txt")]
-#     Cachefile = [
-#         line for line in open("/home/chettyharish/Downloads/Cache.txt")]
+    Storefile = [
+        line for line in open("/home/chettyharish/Downloads/Store.txt")]
+    Cachefile = [
+        line for line in open("/home/chettyharish/Downloads/Cache.txt")]
     Storefile = "/home/chettyharish/Downloads/Store.txt"
     Cachefile = "/home/chettyharish/Downloads/Cache.txt"
     silent_stores = 0.0
@@ -28,16 +156,14 @@ def data_analyzer(start_time, bench, processor, l1_size, l1_assoc, line_size):
     i = 0
     j = 0
     while i < len(Storefile):
-        #addr1 = Storefile[i].split(":")[1]
-        addr1 = linecache.getline(Storefile, i).split(":")[1]
+        addr1 = Storefile[i].split(":")[1]
         while j < len(Cachefile):
-#             addr2 = Cachefile[j].split(":")[1]
-            addr2 = linecache.getline(Cachefile, i).split(":")[1]
+            addr2 = Cachefile[j].split(":")[1]
             if addr1 == addr2:
                 cnt1 = 0
-                size = int(linecache.getline(Cachefile, j + 1).split(":")[1])
-                bfdata = linecache.getline(Cachefile, j + 2).split("\t")
-                afdata = linecache.getline(Cachefile, j + 3).split("\t")
+                size = int(Cachefile[j+1]).split(":")[1]
+                bfdata = Cachefile[j+2].split("\t")
+                afdata = Cachefile[j+3].split("\t")
 
                 for k in range(size):
                     if int(bfdata[k]) == int(afdata[k]):
@@ -119,21 +245,8 @@ def data_analyzer(start_time, bench, processor, l1_size, l1_assoc, line_size):
         #
         ##############################
         printTimeStatement(start_time)
-        silent_store = [1 if ele[1] == ele[2] else 0 for ele in silent_bytes]
-        rle_list = []
-        curr = -1
-        count = 0
-        for i, ele in enumerate(silent_store):
-            if ele == curr:
-                count += 1
-            else:
-                rle_list.append((i, str(curr), str(count)))
-                curr = ele
-                count = 1
-    
         opstring = "\n".join(
-            [str(x[0])+"\t"+str(x[1])+"\t"+str(x[2]) 
-             for x in sorted(rle_list, key=itemgetter(2), reverse=True)[0:200]])
+            [str(x[0])+"\t"+str(x[1])+"\t"+str(x[2]) for x in rle_list])
         outputfile = open(
             "/home/chettyharish/Downloads/" + config_string + ".txt", "w")
         outputfile.write(
@@ -200,5 +313,5 @@ def script_runner():
     return
 
 if __name__ == "__main__":
-    script_runner()
-#     data_analyzer(time.time(),1,1,1,1,1)
+#     script_runner()
+    data_analyzer_smart(time.time(),"hmmr","X86","8kB","4","64")
