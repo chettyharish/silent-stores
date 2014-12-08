df419fed2353fd3ac03ecbf0dd0dd8e42e1254ac
Harish Chetty <chettyharish@gmail.com>
2014-12-07 22:24:36 -0500

    All final outputs

--- outputs/csvCreator.py
+++ outputs/csvCreator.py
@@ -3,16 +3,17 @@ import sys
 
 if __name__ == "__main__":
 	f = []
-	for (dirpath, dirnames, filenames) in os.walk("/home/chettyharish/workspace/temp/silent-stores/outputs"):
+	path = "/home/chettyharish/workspace/temp/silent-stores/outputs/benchmark_RLE"
+	for (dirpath, dirnames, filenames) in os.walk(path):
 	    f.extend(filenames)
-	
+ 	
 	f = [x for x in f if os.path.splitext(x)[1] == ".txt"]
-	
+ 	
 	for file in f:
 		ofn = str(os.path.splitext(file)[0]) + ".csv"
 		print(ofn)
 		ofn = open(ofn , "w")
-		for line in file:
+		for line in open(path + "/" + file , "r"):
 			line = ",".join(line.split()) + "\n"
 			ofn.write(line)
 		ofn.close()
